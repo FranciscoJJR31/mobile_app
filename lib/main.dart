32e6de607ebb72c0d567038a42f490fa3fa58f45
FranciscoJJR31 <javier_3197@hotmail.com>
2022-11-10 15:04:21 -0400

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing   auto-formatting nicer for build methods.
+      ), // This trailing    nicer for build methods.
     );
   }
 }

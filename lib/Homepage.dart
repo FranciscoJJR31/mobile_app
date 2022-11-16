41f251da243342c78e1227d1e5e29df52eb79fe9
FranciscoJJR31 <javier_3197@hotmail.com>
2022-11-15 22:59:57 -0400

    Update

---
+++ lib/Homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+//const HomePage({super.key});
+  String orden;
+  HomePage({super.key, required this.orden});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bienvenido Nuevamente'),
+        ),
+        body: Center(
+          child: Text(orden, style: TextStyle(fontSize: 50)),
+        ),
+      ),
+    );
+  }
+}
